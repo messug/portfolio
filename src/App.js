7933d27bcf59eb254221caf1ef59fb8c497b2140
Meseret <mesiteklu2018@gmail.com>
2024-02-08 21:42:54 -0700

    added the codes

--- src/App.js
+++ src/App.js
@@ -2,29 +2,39 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from './component/Header/Header';
+import Home from './component/Home/Home';
+import Skill from './component/Skills/Skill';
+import Footer from './component/Footer/Footer';
 import DiceGame from './component/Dicegame/DiceGame';
 import About from './component/About/About';
 import Contact from './component/Contacts/Contact';
 import ProjectComponent from './component/Projects/ParentComponent';
+
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './App.css';
 
 function App() {
+  
   return (
+
+    
      <Router>
      <Header />
      <main className="main-content">
        <Routes>
-         {/* <Route path="/" element={<Home />} /> */}
+         <Route path="/home" element={<Home />} />
          <Route path="/about" element={<About />} />
          <Route path="/projects" element={<ProjectComponent />} />
+         <Route path="/skill" element={<Skill />} />
          <Route path="/contact" element={<Contact />} />
+         <Route path="/footer" element={<Footer />} />
          <Route path="/dice-game" component={<DiceGame/>} /> 
        </Routes>
       
      </main>
    </Router>
+
   );
 };
 
---
+++ src/component/Skills/Skill.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import './skill.css'
+export default function Skills(){
+
+  
+  return (
+<section id='skills'>
+  <div className="center">
+   <h2> Skills</h2>
+   <div className="skills-container">
+     <div className="skill-box"><img src="../../images/java-script.png" alt=""/><span>JavaScript</span></div>
+     <div className="skill-box"><img src="../../images/css-3.png" alt=""/><span>CSS</span></div>
+     <div className="skill-box"><img src="../../images/html-5.png" alt=""/><span>HTML</span></div>
+     <div className="skill-box"><img src="../../images/react-logo.png" alt=""/><span>React</span></div>
+     <div className="skill-box"><img src="../../images/mongodb_original_logo_icon_146424.png" alt=""/><span>Mongodb</span></div>
+     <div className="skill-box"><img src="../../images/firebase_logo_icon_171157.png" alt=""/><span>Firebase</span></div>
+     <div className="skill-box"><img src="../../images/nodejs.png" alt=""/><span>Node</span></div>
+     </div>
+   </div>
+</section>
+   );
+}
+  
