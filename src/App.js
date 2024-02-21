a8e456b54c08820f9c979f63b5389850035a3969
Meseret <mesiteklu2018@gmail.com>
2024-02-20 21:24:55 -0700

    modified the codes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from './component/Header/Header';
 // import Home from './component/Home/Home';
 import Skill from './component/Skills/Skill';
 import Footer from './component/Footer/Footer';
-import DiceGame from './component/Dicegame/DiceGame';
+// import DiceGame from './component/Dicegame/DiceGame';
 import About from './component/About/About';
 import Contact from './component/Contacts/Contact';
 import ProjectComponent from './component/Projects/ParentComponent';
@@ -23,13 +23,13 @@ function App() {
      <Header />
      <main className="main-content">
        <Routes>
-         {/* <Route path="/" element={<Home />} /> */}
+         <Route path="/about" element ={<About/>}/>
          <Route path="/about" element={<About />} />
          <Route path="/projects" element={<ProjectComponent />} />
          <Route path="/skill" element={<Skill />} />
          <Route path="/contact" element={<Contact />} />
          <Route path="/footer" element={<Footer />} />
-         <Route path="/dice-game" component={<DiceGame/>} /> 
+         {/* <Route path="/dice-game" component={<DiceGame/>} />  */}
        </Routes>
       
      </main>
