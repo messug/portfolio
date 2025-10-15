1c05e26ff73439bcfec49fa95dfd61a21775e05e
Meseret <mesiteklu2018@gmail.com>
2025-10-15 10:09:02 -0600

    Center home page content and update email

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <section className='App'>
+   <div > 
+    <header className='header'>
+       <h1>About</h1>
+    </header>
+   </div>
+   </section>
+    </>
+  )
+}
+
+export default App
